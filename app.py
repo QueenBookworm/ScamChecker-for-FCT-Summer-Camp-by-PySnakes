1b3773bdf64d97310819e7192e328a241a58b36a
Ensethyne <125657950+QueenBookworm@users.noreply.github.com>
2026-06-18 23:20:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,61 +12,67 @@ def index():
         platform = request.form.get('platform')
         number = request.form.get('number')
         agency = request.form.get('company')
-        # original_message = "Prompt"
-        ai_input = []
-        if platform:
-            ai_input.append(types.Part.from_text(text=f"Message was sent using {platform}."))
-        if number:
-            ai_input.append(types.Part.from_text(text=f"The user {number} this number."))
-        if agency:
-            ai_input.append(types.Part.from_text(text=f"The user {agency} this company."))
-        if message:
-            ai_input.append(types.Part.from_text(text = message))
-        if scam_image and scam_image.filename:
-            image_bytes = scam_image.read()
-            ai_input.append(
-                types.Part.from_bytes(
-                    data=image_bytes,
-                    mime_type=scam_image.content_type,
-                )
-            )
-        if ai_input:
-            try:
-                client = genai.Client(api_key="")
-                print(ai_input) # We wanna check if this works, how it looks
-                response = client.models.generate_content(
-                        model='gemini-3.1-flash-lite',
-                        contents=ai_input,
-                        config=types.GenerateContentConfig(
-                            system_instruction="You are an evidence-driven assistant that assesses whether a message or attached image is likely a scam. Consider message content, sender info, platform, and image. Return a balanced integer 'scam_score' from 0 (not a scam) to 100 (definite scam). If uncertain, reflect that with a mid-range score (e.g., 40–60). Provide 'reason' in 20–50 words summarizing the key evidence and uncertainty, and 'action' in under 20 words advising the user. Output ONLY valid JSON with keys: {'scam_score': int, 'reason': str, 'action': str}. Do not include any extra explanation, markup, or backticks. Avoid defaulting to high scam scores; weigh both indicators and counter-indicators carefully."),
-                        )
-                            
-                clean_txt = response.text.strip().replace("```json", "").replace("```", "")
-                data = json.loads(clean_txt)
-                scam_score = int(data["scam_score"])
-                response_text = f"{data['reason']} {data['action']}"
-                status = "You are safe" if scam_score < 50 else "You have been scammed"
-                history_list.append({"status":status,
-                                "scam_score":scam_score,
-                                "message":response_text,
-                                "original_message":request.form.get('message')})
-                if len(history_list) > 10:
-                    history_list.pop(0)
-            except Exception as e:
-                response_text = f"An error occurred: {str(e)}"
-                scam_score = 0
-                status = "Error"
+        if not message and not (scam_image and scam_image.filename):
+            return render_template("result.html", status="Please enter a message or upload an image.", scam_score=100, color="#f56020")
         else:
-            response_text = "No input provided"
-            scam_score = 0
-            status = "Error, please try again."
-        return render_template(
-            "result.html",
-            status=status,
-            scam_score=scam_score,
-            message=response_text,
-            original_message=request.form.get('message')
-        )
+            ai_input = []
+            if platform:
+                ai_input.append(types.Part.from_text(text=f"Message was sent using {platform}."))
+            if number:
+                ai_input.append(types.Part.from_text(text=f"The user {number} know this number."))
+            if agency:
+                ai_input.append(types.Part.from_text(text=f"The user {agency} company."))
+            if message:
+                ai_input.append(types.Part.from_text(text = message))
+            if scam_image and scam_image.filename:
+                image_bytes = scam_image.read()
+                ai_input.append(
+                    types.Part.from_bytes(
+                        data=image_bytes,
+                        mime_type=scam_image.content_type,
+                    )
+                )
+            if ai_input:
+                try:
+                    client = genai.Client(api_key="")
+                    print(ai_input) # We wanna check if this works, how it looks
+                    response = client.models.generate_content(
+                            model='gemini-3.1-flash-lite',
+                            contents=ai_input,
+                            config=types.GenerateContentConfig(
+                                system_instruction="You are an evidence-driven assistant that assesses whether a message or attached image is likely a scam. Consider message content, sender info, platform, and image. Return a balanced integer 'scam_score' from 0 (not a scam) to 100 (definite scam). If uncertain, reflect that with a mid-range score (e.g., 40–60). Provide 'reason' in 20–50 words summarizing the key evidence and uncertainty, and 'action' in under 20 words advising the user. Output ONLY valid JSON with keys: {'scam_score': int, 'reason': str, 'action': str}. Do not include any extra explanation, markup, or backticks. Avoid defaulting to high scam scores; weigh both indicators and counter-indicators carefully."),
+                            )
+                                
+                    clean_txt = response.text.strip().replace("```json", "").replace("```", "")
+                    data = json.loads(clean_txt)
+                    scam_score = int(data["scam_score"])
+                    response_text = f"{data['reason']} {data['action']}"
+                    status = "You are safe" if scam_score < 50 else "You have been scammed"
+                    color = "#f56020" if scam_score <60 else "#E50B31"
+                    history_list.append({"status":status,
+                                    "scam_score":scam_score,
+                                    "message":response_text,
+                                    "original_message":request.form.get('message'),
+                                    "color":color})
+                    print(color)
+                    if len(history_list) > 10:
+                        history_list.pop(0)
+                except Exception as e:
+                    response_text = f"An error occurred: {str(e)}"
+                    scam_score = 0
+                    status = "Error"
+            else:
+                response_text = "No input provided"
+                scam_score = 0
+                status = "Error, please try again."
+            return render_template(
+                "result.html",
+                status=status,
+                scam_score=scam_score,
+                message=response_text,
+                original_message=request.form.get('message'),
+                color=color
+            )
     return render_template('index.html')
 
 @app.route("/history")
@@ -82,4 +88,4 @@ def result():
 #     return render_template('rreas# teregg.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=5001, debug=True)
